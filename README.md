c892b4db42641076c362f26b0e410ce4daa7dcd0
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2024-04-01 18:09:35 -0700

    ahmed

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-create a script that creates an alias
-readme.md
 readme.me
--- additionla/main.py
+++
@@ -1,9 +0,0 @@
-from decode_message import decode
-
-def main():
-    input_file = "my.txt"  # Replace with the path to your input file
-    decoded_message = decode(input_file)
-    print("Decoded message:", decoded_message)
-
-if __name__ == "__main__":
-    main()
